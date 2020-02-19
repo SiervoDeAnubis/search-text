a828750d6144622ce0a9c05bef947f2c33b88e52
César Olayo Espíritu <omnislahs@hotmail.com>
2020-02-18 23:43:45 -0600

    just vanilla js

--- vanilla/main.js
+++ vanilla/main.js
@@ -1,20 +1,36 @@
-const API_URL = "https://api.unsplash.com/search/photos?page=1&client_id={your_key}&query="
-
-console.log("hello world!!!");
+const API_URL =
+  "https://api.unsplash.com/search/photos?page=1&client_id={you_key}";
 
 const form = document.querySelector("form");
 const searchInput = document.querySelector("input");
+const loadingImage = document.querySelector("#loadingImage");
+const images = document.querySelector(".images");
+
+loadingImage.style.display = "none";
 
 form.addEventListener("submit", formSubmitted);
 
 function formSubmitted(event) {
   event.preventDefault();
   const searchTerm = searchInput.value;
-  search(searchTerm);
+  search(searchTerm).then(displayImages);
 }
 
 function search(term) {
-  const url = `${API_URL}${term}`;
-  console.log(url)
-  fetch(url).then(response => response.json()).then(results => console.log(results));
+  const url = `${API_URL}&query=${term}`;
+  loadingImage.style.display = "";
+  return fetch(url)
+    .then(response => response.json())
+    .then(results => {
+      return results;
+    });
+}
+
+function displayImages({ results }) {
+  for (let index in results) {
+    const imageElement = document.createElement("img");
+    imageElement.src = results[index].urls.regular;
+    images.appendChild(imageElement);
+  }
+  loadingImage.style.display = "none";
 }
